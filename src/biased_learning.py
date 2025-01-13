bdc5acc4742579dd2efdc58eb3a892ff507ba20c
Rahul Gedela <gedelaramasairahul@gmail.com>
2025-01-13 12:31:56 -0500

    Removed unused library imports. Fixed feature vector logic (hopefully)

--- src/biased_learning.py
+++ src/biased_learning.py
@@ -2,7 +2,6 @@ import numpy as np
 
 from sklearn.preprocessing import LabelEncoder
 
-import tensorflow as tf
 from keras import Sequential
 from keras.src.layers import Dense, Dropout
 
@@ -19,8 +18,14 @@ y_train_encoded=label_encoder.fit_transform(y_train) #Encode to numerical
 unique_labels, counts=np.unique(y_train, return_counts=True) #Unique labels (students) and their counts of submissions
 submission_weights={label: count for label, count in zip(unique_labels, counts)} #Map each label to count
 
-weights=np.array([submission_weights[label] for label in y_train])
-extracted_features_weighted=extracted_features*weights[:, np.newaxis]
+total_submissions=sum(submission_weights.values())
+normalized_weights={label: submission_weight/total_submissions for label, submission_weight in submission_weights.items()}
+
+feature_vector=np.zeros_like(extracted_features)
+for i, label in enumerate(y_train):
+    student_weight=normalized_weights[label]
+    feature_vector+=student_weight*extracted_features[i]
+print('Feature vector computed!')
 
 model=Sequential([
     Dense(128, activation='relu', input_shape=(extracted_features.shape[1],)),
@@ -33,7 +38,7 @@ model.compile(optimizer='Adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(extracted_features_weighted, y_train_encoded, epochs=20, batch_size=32, validation_split=0.2)
+model.fit(feature_vector, y_train_encoded, epochs=20, batch_size=32, validation_split=0.2)
 
 model_save_path='../models/biased_learning_model.h5'
 model.save(model_save_path)
